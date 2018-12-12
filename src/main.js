0c4b1ba5d4686b5a0e4f7ba123952fedd313d04b
Mike Miller <himikemiller@gmail.com>
2018-12-12 15:32:01 -0500

    test build 3

--- src/main.js
+++ src/main.js
@@ -2,11 +2,11 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import {
-  Layout
-} from 'bootstrap-vue/es/components';
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-Vue.use(Layout);
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
 
